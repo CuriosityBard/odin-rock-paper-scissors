ef51f97e5616d7b36eafac8937478ed03fcf1226
CuriosityBard <curiositybard@gmail.com>
2022-06-20 10:09:52 -0700

    Add player selection function

--- scripts.js
+++ scripts.js
@@ -1,6 +1,12 @@
 function computerPlay() {
     let randomNum = Math.floor(Math.random() * 3);
     // use random number to choose a sign
-    let whichOne = randomNum === 0 ? 'Rock' : randomNum === 1 ? 'Paper' : 'Scissors'; 
-    return whichOne;
+    let computerChoice = randomNum === 0 ? 'Rock' : randomNum === 1 ? 'Paper' : 'Scissors'; 
+    return computerChoice;
+}
+
+function playerChoice() {
+    playerChoice = prompt("Please enter rock, paper, or scissors.");
+    // return the response with only the first letter capitalized
+    return playerChoice.slice(0,1).toUpperCase() + playerChoice.slice(1).toLowerCase();
 }
