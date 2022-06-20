84046800eced041c2616c25097e5ddce12691363
CuriosityBard <curiositybard@gmail.com>
2022-06-20 10:10:18 -0700

    Add main function to compare choices

--- scripts.js
+++ scripts.js
@@ -10,3 +10,23 @@ function playerChoice() {
     // return the response with only the first letter capitalized
     return playerChoice.slice(0,1).toUpperCase() + playerChoice.slice(1).toLowerCase();
 }
+
+function compareChoices(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return `It's a tie! You both chose ${playerSelection}`;
+    } else if (playerSelection === 'Rock' && computerSelection === 'Paper') {
+        return 'You lose! Paper beats Rock.';
+    } else if (playerSelection === 'Paper' && computerSelection === 'Rock') {
+        return 'You win! Paper beats Rock.';
+    } else if (playerSelection === 'Rock' && computerSelection === 'Scissors') {
+        return 'You win! Rock beats Scissors.';
+    } else if (playerSelection === 'Scissors' && computerSelection === 'Rock') {
+        return 'You lose! Rock beats Scissors.';
+    } else if (playerSelection === 'Paper' && computerSelection === 'Scissors') {
+        return 'You lose! Scissors beat Paper.';
+    } else if (playerSelection === 'Scissors' && computerSelection === 'Rock') {
+        return 'You win! Scissors beat Paper.';
+    }  else {
+        return 'Something went wrong!';
+    }
+}
