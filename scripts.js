d111b1fa44efbd204b18814a37616f1e2db32dee
CuriosityBard <curiositybard@gmail.com>
2022-06-25 10:37:56 -0700

    Game plays to five points and resets after

--- scripts.js
+++ scripts.js
@@ -6,22 +6,60 @@ const domElements = {
     playerResult: document.querySelector('#player-result'),
     computerResult: document.querySelector('#computer-result'),
     gameResult: document.querySelector('#game-result'),
+
+    playerResultArea: document.querySelector('#player-result-area'),
+    computerResultArea: document.querySelector('#computer-result-area'),
+
+    hideResults: function() {
+        this.playerResultArea.classList.add('hide');
+        this.computerResultArea.classList.add('hide');
+    }
 }
 
 const scoring = {
     playerScore: 0,
     computerScore: 0,
+
+    firstRound: true,
+
+    checkForWin: function() {
+        if (this.playerScore === 5) {
+            domElements.gameResult.textContent = "Player wins the game!"
+            domElements.gameResult.classList.add('game-over');
+
+            domElements.hideResults();
+            this.firstRound = true;
+        } else if (this.computerScore === 5) {
+            domElements.gameResult.textContent = "Computer wins the game!"
+            domElements.gameResult.classList.add('game-over');
+
+            domElements.hideResults();
+            this.firstRound = true;
+        }
+    },
+
     incrementPlayerScore: function() {
         domElements.gameResult.textContent = "Player wins this round!";
 
         this.playerScore++;
         domElements.playerScore.textContent = this.playerScore;
+
+        this.checkForWin();
     },
     incrementComputerScore: function() {
         domElements.gameResult.textContent = "Computer wins this round!";
 
         this.computerScore++;
         domElements.computerScore.textContent = this.computerScore;
+
+        this.checkForWin();
+    },
+
+    resetScores: function() {
+        this.playerScore = 0;
+        this.computerScore = 0;
+        domElements.computerScore.textContent = this.computerScore;
+        domElements.playerScore.textContent = this.playerScore;
     }
 }
 
@@ -29,7 +67,6 @@ function computerPlay() {
     let randomNum = Math.floor(Math.random() * 3);
     // use random number to choose a sign
     let computerChoice = randomNum === 0 ? 'Rock' : randomNum === 1 ? 'Paper' : 'Scissors'; 
-    console.log(computerChoice);
     return computerChoice;
 }
 
@@ -46,6 +83,16 @@ function chooseScissors() {
 function playRound(playerSelection, computerSelection) {
     domElements.playerResult.textContent = playerSelection;
     domElements.computerResult.textContent = computerSelection;
+
+    // remove 'hide' class from player and computer results if it's the first round
+    if (scoring.firstRound) {
+        domElements.playerResultArea.classList.remove('hide');
+        domElements.computerResultArea.classList.remove('hide');
+
+        scoring.resetScores();
+
+        scoring.firstRound = false;
+    }
 
     if (playerSelection === computerSelection) {
         domElements.gameResult.textContent = "It's a tie!";
